c3273d4efd2739cc8b53b711175c11a937fff0f2
Jurre Stender <jurrestender@hotmail.com>
2013-03-19 10:22:24 +0100

    clean up

--- crashlytics-asana.rb
+++ crashlytics-asana.rb
@@ -11,10 +11,10 @@ set :protection, :except => [:http_origin]
 post '/' do
   json_string = request.body.read.to_s
   
-  puts JSON.parse(json_string)['event']
   # respond with a 200 to the Crashlytics verification request
   return 'ok' if JSON.parse(json_string)['event']  == 'verification'
   
+  # post to asana
   post = CrashlyticsPost.from_json(json_string)
   post.create_asana_task!
 end
